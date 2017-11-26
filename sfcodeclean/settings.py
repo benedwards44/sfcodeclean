c2562840b5501ea8a8beeb909a50be4fabb794aa
Ben Edwards <ben@edwards.nz>
2017-11-26 16:42:30 +1300

    Static files issues

--- sfcodeclean/settings.py
+++ sfcodeclean/settings.py
@@ -159,7 +159,7 @@ STATIC_URL = '/static/'
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 SALESFORCE_CONSUMER_KEY = os.environ.get('SALESFORCE_CONSUMER_KEY')
 SALESFORCE_CONSUMER_SECRET = os.environ.get('SALESFORCE_CONSUMER_SECRET')
